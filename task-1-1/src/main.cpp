75e4c760821f4a45ca061e287ef171cf23c736d7
VDN <114281516+Nikolaevviktord@users.noreply.github.com>
2025-07-01 22:47:55 +0300

    Create main.cpp

---
+++ task-1-1/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+const int ledPin = 2;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, not digitalRead(ledPin));
+  delay(500);
+}
+  
