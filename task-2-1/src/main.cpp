c343562d7faa65e9daed7480724cf218cdb15e2c
VDN <114281516+Nikolaevviktord@users.noreply.github.com>
2025-07-01 23:03:58 +0300

    Create main.cpp

---
+++ task-2-1/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+const auto ledPin = 26, butPin = 14;
+
+void setup() {
+    // Настройка режимов пинов
+    pinMode(ledPin, OUTPUT);
+    pinMode(butPin, INPUT_PULLUP);
+}
+
+void loop() {
+    bool button_state = digitalRead(butPin);
+    digitalWrite(ledPin, button_state);
+
+    delay(100);
+}
