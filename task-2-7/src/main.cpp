258b11720e69f8ba7110adc4a44863f4c3e213c0
VDN <114281516+Nikolaevviktord@users.noreply.github.com>
2025-07-03 12:33:32 +0300

    Create main.cpp

---
+++ task-2-7/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+const auto pin_led = 26;
+
+const auto pwm_resolution = 10;  // Разрешение ШИМ (10 бит = 0-1023)
+
+void setup() {
+    pinMode(pin_led, OUTPUT);
+    
+    // Настройка параметров ШИМ
+    analogWriteResolution(pwm_resolution);
+    analogWriteFrequency(10000);  // Частота 10 КГц
+}
+
+void loop() {
+    const auto max_value = (1 << pwm_resolution) - 1;  // Максимальное значение
+    
+    // Плавное изменение яркости по треугольной функции
+    for (int i = -max_value; i < max_value; i++) {
+        auto pwm_value = max_value - abs(i);  // Треугольная функция
+        analogWrite(pin_led, pwm_value);
+        delay(1000 / max_value);  // Плавное изменение
+    }
+}
