d4dbff0b974eafb6757ab0d8417ea4c24c8b5b02
VDN <114281516+Nikolaevviktord@users.noreply.github.com>
2025-07-03 10:52:50 +0300

    Update main.cpp

--- task-1-3/src/main.cpp
+++ task-1-3/src/main.cpp
@@ -1,10 +1,13 @@
-const int ledPin = 2;
+#include <Arduino.h>
+
+const int ledPin = 2; // пин светолиода
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT); // объявляем пин светодиода как выход
 }
 
 void loop() {
+  // мигаем светодиодом
   digitalWrite(ledPin, not digitalRead(ledPin));
   delay(500);
 }
